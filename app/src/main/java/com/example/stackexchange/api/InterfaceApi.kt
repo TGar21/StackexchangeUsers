fbfc6afd0c5374cafbad3ef08e65e59f6d411fe0
Tadeáš Friedrich <tadeas.friedrich@gmail.com>
2022-01-21 21:36:37 +0100

    Run first placeholder StackExchange request to test the API

--- app/src/main/java/com/example/stackexchange/api/InterfaceApi.kt
+++ app/src/main/java/com/example/stackexchange/api/InterfaceApi.kt
@@ -1,4 +1,44 @@
 package com.example.stackexchange.api
 
-class InterfaceApi {
+import android.util.Log
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+interface InterfaceApi {
+    @GET("/2.3/users?order=desc&sort=reputation&inname=Vitinger&site=stackoverflow&filter=!0Z-LvhbSNwb7VsNYilJam(tyY")
+    fun getData(): Call<UserData>
+
+
+    companion object {
+        var BASE_URL = "https://api.stackexchange.com"
+
+        fun create(): InterfaceApi {
+            val retrofitBuilder = Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(BASE_URL)
+                .build()
+                .create(InterfaceApi::class.java)
+
+            val retrofitData = retrofitBuilder.getData()
+
+            retrofitData.enqueue(object : Callback<UserData> {
+                override fun onResponse(
+                    call: Call<UserData>,
+                    response: Response<UserData>
+                ) {
+                    val responseBody = response.body()
+                    Log.d("myTag", "response: $responseBody")
+                }
+
+                override fun onFailure(call: Call<UserData>, t: Throwable) {
+                    Log.d("myTag", "failure: ${t.localizedMessage}")
+                }
+            })
+            return retrofitBuilder
+        }
+    }
 }

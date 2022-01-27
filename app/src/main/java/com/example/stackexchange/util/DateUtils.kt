541d67429310ad73799c5a667c9ce958c7a43aea
Tadeas <tadeas.friedrich@gmail.com>
2022-01-27 01:15:58 +0100

    Create user detail custom view

--- app/src/main/java/com/example/stackexchange/util/DateUtils.kt
+++ app/src/main/java/com/example/stackexchange/util/DateUtils.kt
@@ -1,4 +1,11 @@
 package com.example.stackexchange.util
 
+import java.text.SimpleDateFormat
+import java.util.*
+
 class DateUtils {
+    fun millisToDate(millis: Long) {
+        val formatter = SimpleDateFormat("dd/MM/yyyy")
+        val dateString = formatter.format(Date(millis))
+    }
 }
